c6cd6b1bfe4215dc11a1db4334eb4aa507504220
srirajyt <143815834+srirajyt@users.noreply.github.com>
2023-12-15 00:10:22 +0530

    Add files via upload

---
+++ Projectile.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+class USoundbase;
+
+UCLASS()
+class TOONTANKS_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* ProjectileMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Movement")
+	class UProjectileMovementComponent* ProjectileMovement;
+	
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	UPROPERTY(Editanywhere, Category = "Combat")
+	float Damage = 50.f;
+
+	UPROPERTY(Editanywhere, Category = "Combat")
+	class UParticleSystem* HitParticles;
+
+	UPROPERTY(Visibleanywhere, Category = "Combat")
+	class UParticleSystemComponent* BulletTrail;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	USoundBase* LaunchSound;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	USoundBase* HitSound;
+
+	UPROPERTY(Editanywhere, Category = "Combat")
+	TSubclassOf<class UCameraShakeBase> HitCameraShakeclass;
+
+};
